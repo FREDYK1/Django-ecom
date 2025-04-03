8b2007ed07e1d1b903d6fe9e549ee13c7249c4a8
FREDYK1 <fkwekukankam@gmail.com>
2025-04-03 16:43:30 +0000

    Create Product Category Pages

--- store/models.py
+++ store/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import datetime
 
 class Category(models.Model):
-    name= models.CharField(max_length=40)
+    name = models.CharField(max_length=40)
 
     def __str__(self):
         return self.name
